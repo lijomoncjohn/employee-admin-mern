674fa72041784ed264b9e89d2f188c5b313b2df6
Lijomon C John <lijomon@promasoft.net>
2021-07-24 23:11:50 +0530

    Contet wrapped

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,18 +4,43 @@ import { ROUTES } from './core/base/constants';
 
 import Login from './containers/Auth/Login';
 import Layout from './layout/Layout';
+import { useAuth } from './core/context/authHook';
+import Dashboard from './containers/Dashboard/Dashboard';
+import { AuthContext } from './core/context/authContext';
 
 function App() {
-	const routes = (
-		<Switch>
-			<Route path={ROUTES.HOME} component={Login} exact />
-			<Route path='*' component={Login} />
-		</Switch>
-	);
+	const { token, userId, login, logout } = useAuth();
+
+	let routes;
+
+	if (token) {
+		routes = (
+			<Switch>
+				<Route path={ROUTES.DASHBOARD} component={Dashboard} exact />
+				<Route path='*' component={Dashboard} />
+			</Switch>
+		);
+	} else {
+		routes = (
+			<Switch>
+				<Route path={ROUTES.HOME} component={Login} exact />
+				<Route path='*' component={Login} />
+			</Switch>
+		);
+	}
 	return (
-		<Router>
-			<Layout isAuthorised={true}>{routes}</Layout>
-		</Router>
+		<AuthContext.Provider
+			value={{
+				isLoggedIn: !!token,
+				token: token,
+				userId: userId,
+				login: login,
+				logout: logout,
+			}}>
+			<Router>
+				<Layout isAuthorised={!!token}>{routes}</Layout>
+			</Router>
+		</AuthContext.Provider>
 	);
 }
 
