88292394c568875f5be428a8d0003476461e9c19
Lijomon C John <lijomon@promasoft.net>
2021-07-25 01:28:44 +0530

    Added API wrapper for networking with axios

--- frontend/src/core/base/constants.js
+++ frontend/src/core/base/constants.js
@@ -2,3 +2,15 @@ export const ROUTES = {
 	DASHBOARD: '/dashboard',
 	HOME: '/',
 };
+
+export const ApiMethods = {
+	POST: 'POST',
+	GET: 'GET',
+	PUT: 'PUT',
+	DELETE: 'DELETE',
+};
+
+export const HTTP_HEADER = {
+	Authorization: 'Authorization',
+	ContentType: 'Content-Type',
+};
